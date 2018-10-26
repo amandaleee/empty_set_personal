b7a78337d831e60a5ada6ba5637ca2cead589d87
Tim Rourke <trourke@activecampaign.com>
2018-10-26 10:38:24 -0500

    Use Coverage for test coverage reports

--- empty_set/settings.py
+++ empty_set/settings.py
@@ -118,11 +118,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Use unittest-xml-reporting for generating test coverage reports
-# https://github.com/xmlrunner/unittest-xml-reporting#django
-TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
-
-TEST_OUTPUT_VERBOSE = 2
-
-TEST_OUTPUT_DIR = './test-results'
